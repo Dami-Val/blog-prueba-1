fe240bd68b651d6ad412dffcebe6182a71a66bdf
Damian <damival.32@gmail.com>
2025-02-25 18:40:11 -0600

    a

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav>
+      <h1>Blog Navbar</h1>
+      {/* ...existing code... */}
+    </nav>
+  );
+};
+
+export default Navbar;
